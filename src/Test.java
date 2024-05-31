3b0318820757c791d6bea0e350dcdf6b3e64b464
hyezuu <duudu0704@gmail.com>
2024-05-31 13:07:36 +0900

    update

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Test {
   public static void main(String[] args) {
@@ -35,6 +36,21 @@ public class Test {
     String b = "WEEEE ASKE ;ND";
     System.out.println(b.substring(0,3)+b.substring(8,10)+b.substring(12,14));
 
+    String c = "a:b:c:d";
+    StringTokenizer st = new StringTokenizer(c,":");
+    while (st.hasMoreTokens()){
+      System.out.println(st.nextToken());
+    }
+    String[] split = a.split(":");
+    System.out.println(String.format("%d...%s...%b...%c",10,"안녕",2,'a'));
 
+    String name1 = "Tim";
+    int age1 = 30;
+
+    String name2 = "Anna";
+    int age2 = 45;
+
+    String.format("$s의 나이는 %s세 입니다", name1, age1);
+    String.format("$s의 나이는 %s세 입니다", name2, age2);
   }
 }
