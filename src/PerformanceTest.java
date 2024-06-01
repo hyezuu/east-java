051fe1d5df9c58cb3b3131356216fe4c9bb954c8
hyezuu <duudu0704@gmail.com>
2024-06-01 13:28:00 +0900

    update

--- src/PerformanceTest.java
+++ src/PerformanceTest.java
@@ -1,23 +1,37 @@
 public class PerformanceTest {
-    public static void main(String[] args) {
-        long startTime, endTime;
+  public static void main(String[] args) {
+    //        long startTime, endTime;
+    //
+    //        // StringBuilder 성능 테스트
+    //        StringBuilder sb = new StringBuilder();
+    //        startTime = System.nanoTime();
+    //        for (int i = 0; i < 100000; i++) {
+    //            sb.append("test");
+    //        }
+    //        endTime = System.nanoTime();
+    //        System.out.println("StringBuilder 걸린 시간: " + (endTime - startTime) + " ns");
+    //
+    //        // StringBuffer 성능 테스트
+    //        StringBuffer sbf = new StringBuffer();
+    //        startTime = System.nanoTime();
+    //        for (int i = 0; i < 100000; i++) {
+    //            sbf.append("test");
+    //        }
+    //        endTime = System.nanoTime();
+    //        System.out.println("StringBuffer 걸린 시간: " + (endTime - startTime) + " ns");
 
-        // StringBuilder 성능 테스트
-        StringBuilder sb = new StringBuilder();
-        startTime = System.nanoTime();
-        for (int i = 0; i < 100000; i++) {
-            sb.append("test");
-        }
-        endTime = System.nanoTime();
-        System.out.println("StringBuilder 걸린 시간: " + (endTime - startTime) + " ns");
+    //    int[] arr = new int[12];
+    //
+    //    System.out.println(arr);
+    //    System.out.println(System.identityHashCode(arr[0]));
+    //    System.out.println(System.identityHashCode(arr[1]));
+    //    System.out.println(System.identityHashCode(arr[2]));
 
-        // StringBuffer 성능 테스트
-        StringBuffer sbf = new StringBuffer();
-        startTime = System.nanoTime();
-        for (int i = 0; i < 100000; i++) {
-            sbf.append("test");
-        }
-        endTime = System.nanoTime();
-        System.out.println("StringBuffer 걸린 시간: " + (endTime - startTime) + " ns");
+    String[] arr = new String[100];
+    try {
+      System.out.println(arr[0].length());
+    } catch (NullPointerException e) {
+      throw new RuntimeException("널 위한 예외처리 ㅋ");
     }
+  }
 }
